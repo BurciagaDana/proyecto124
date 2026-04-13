f5f0a875db72e2ef2581674b49eccdc5b1817648
BurciagaDana <23308060610289@cetis61.edu.mx>
2026-04-13 09:02:14 -0500

    fnfnfnnfnfnh

--- models/UserModel.py
+++ models/UserModel.py
@@ -8,4 +8,30 @@ class UsuarioModel:
         def registrar(self, usuario_data):
             #Encriptar contraseña
             salt = bcrypt.gensalt()
-            hashed_pw = bcrypt.hashpw(usuario_data.password.encode())
+            hashed_pw = bcrypt.hashpw(usuario_data.password.encode('utf_8'), salt)
+            
+            conn = self.db.get_connection()
+            cursor = conn.cursor()
+            try: 
+                cursor.execute(
+                    "INSERT INTO usuario (nombre, email, password) VALUES (%s, %s, %s)", 
+                    (usuario_data.nombre, usuario_data.email, hashed_pw.decode('utf-8'))
+                    
+                )
+                conn.commit()
+                return True
+            except Exception as e:
+                print(f"Error: {e}")
+            finally:
+                conn.close()
+                
+def validar_login(self, email, password):
+    conn = self.db.get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT = FROM usuario WHERE email= %s", (email))
+    user = cursor.fetchone()
+    conn.closer()
+    
+    if user and bcrypt.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
+        return user
+    return None                    
