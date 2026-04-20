57c0abad369bcab28772a7ea88560409bfda0f27
BurciagaDana <23308060610289@cetis61.edu.mx>
2026-04-20 08:43:15 -0500

    oli

--- src/main.py
+++ src/main.py
@@ -23,8 +23,15 @@ def start(page: ft.Page):
             )
 
         page.update()
+        
+    def view_pop(e):
+        if len(page.views) > 1:
+            page.views.pop()
+            top_view = page.view[-1]
+            page.go(top_view.route)
 
     page.on_route_change = route_change
+    page.on_view_pop = view_pop
     page.go("/")
 
 def main():
